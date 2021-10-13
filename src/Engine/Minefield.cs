08dd57b25a2466cf727b89bdf53f080315b453ed
Mark Roberts <mroberts@geodesys.com>
2021-10-13 21:57:07 +0100

    Made implementation of GetHint clearer
    We can generate the list of cells adjacent to the cell we're checking for. That makes it easier to filter off cells that don't appear in our minefield at that point, rather than when we're counting how many of those are mined.
    
    By introducing overloads of e.g. `HasCell`, `IsMined`, and `GetHint` that accept a `Cell` rather than a row and column we can further shorten the lambda expressions as we don't have to construct a `Cell` in them now.

--- src/Engine/Minefield.cs
+++ src/Engine/Minefield.cs
@@ -16,29 +16,33 @@ namespace Dgt.Minesweeper.Engine
 
         public int Columns { get; }
         public int Rows { get; }
-        
-        public bool IsMined(int column, int row)
-        {
-            return _minedCells.Contains(new Cell(column, row));
-        }
-        
-        public int GetHint(int column, int row)
-        {
-            List<Cell> adjacentCells = new()
-            {
-                new Cell(column - 1, row - 1),
-                new Cell(column - 1, row),
-                new Cell(column - 1, row + 1),
-                new Cell(column, row - 1),
-                new Cell(column, row + 1),
-                new Cell(column + 1, row - 1),
-                new Cell(column + 1, row),
-                new Cell(column + 1, row + 1)
-            };
 
-            return adjacentCells.Count(cell => HasCell(cell.Column, cell.Row) && IsMined(cell.Column, cell.Row));
-        }
+        public bool IsMined(Cell cell) => _minedCells.Contains(cell);
+
+        public bool IsMined(int column, int row) => IsMined(new Cell(column, row));
+
+        public int GetHint(Cell cell) => GetAdjacentCells(cell).Count(IsMined);
+
+        public int GetHint(int column, int row) => GetHint(new Cell(column, row));
         
+        private IEnumerable<Cell> GetAdjacentCells(Cell cell)
+        {
+            for (var column = cell.Column - 1; column <= cell.Column + 1; column++)
+            {
+                for (var row = cell.Row - 1; row <= cell.Row + 1; row++)
+                {
+                    var currentCell = new Cell(column, row);
+                    
+                    if (HasCell(currentCell) && currentCell != cell)
+                    {
+                        yield return new Cell(column, row);
+                    }
+                }
+            }
+        }
+
+        private bool HasCell(Cell cell) => HasCell(cell.Column, cell.Row);
+
         private bool HasCell(int column, int row) => column <= Columns && row <= Rows;
     }
 }
