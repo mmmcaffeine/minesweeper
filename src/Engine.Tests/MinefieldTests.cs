82f9109c6a7674ac9e1c8baffd3f128f299b0d07
Mark Roberts <mroberts@geodesys.com>
2021-10-13 18:31:44 +0100

    Added more test cases for the Minefield.GetHint method.
    This was achieved by using `Theory` for the existing test, and some `InlineData`, rather than just a `Fact`.
    
    This code is supposed to be a fair reflection of the process I went through when first presented with this problem, rather than just showing what the final version of the code was.

--- src/Engine.Tests/MinefieldTests.cs
+++ src/Engine.Tests/MinefieldTests.cs
@@ -8,14 +8,20 @@ namespace Dgt.Minesweeper.Engine
         // TODO What should the hint for a mined square be?
         // The example input shows "3, 4" meaning 3 rows, amd 4 columns. Our origin is the bottom left, and our coordinate
         // system is 1-based i.e. how the vast majority of the population would think of a minesweeper grid
-        [Fact]
-        public void GetHint_Should_ReturnNumberOfAdjacentMines()
+        [Theory]
+        [InlineData(3, 2, 2)]
+        [InlineData(1, 1, 0)]
+        [InlineData(1, 4, 1)]
+        public void GetHint_Should_ReturnNumberOfAdjacentMines(int row, int column, int expectedHint)
         {
+            // Arrange
             var minefield = new Minefield(3, 4, new[] { new Square(3, 1), new Square(2, 3) });
 
-            var hint = minefield.GetHint(3, 2);
-
-            hint.Should().Be(2);
+            // Act
+            var hint = minefield.GetHint(row, column);
+            
+            // Assert
+            hint.Should().Be(expectedHint);
         }
     }
 }
