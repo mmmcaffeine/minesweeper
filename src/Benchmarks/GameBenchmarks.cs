b04b4817b0143dd665ae3d2864146f41a6e5e033
Mark Roberts <mroberts@geodesys.com>
2021-10-24 11:34:05 +0100

    Wrote benchmarks to explore four ways for generating an enumerable of cells
    Most of the time we only have the number of columns and the number of rows. We need to turn this into an enumerable of cells. There are four options I could think of:
    
    * A nested `for` loop
    * A nested `foreach` loop
    * A `SelectMany` call
    * A query expression
    
    Although the nested `for` loop has a bit more code going on it proved to be the fastest in all four scenarios. I would assume a significant proportion of that is being the only option that _doesn't_ need to generate two `IEnumerable<int>`s.
    
    The end result of all that is not needing to change the loop I had in place! 😂

---
+++ src/Benchmarks/GameBenchmarks.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using Dgt.Minesweeper.Engine;
+
+namespace Dgt.Minesweeper.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class GameBenchmarks
+    {
+        private static readonly Consumer Consumer = new();
+        
+        public static IEnumerable<int> ValuesForNumberOfRowsAndColumns
+        {
+            get
+            {
+                yield return 2;
+                yield return 10;
+                yield return 100;
+                yield return 1000;
+            }
+        }
+
+        [ArgumentsSource(nameof(ValuesForNumberOfRowsAndColumns))]
+        [Benchmark]
+#pragma warning disable CA1822
+        public void GetCells_Using_NestedForLoop(int numberOfRowsAndColumns)
+#pragma warning restore CA1822
+        {
+            DoGetCellsUsingNestedForLoop(numberOfRowsAndColumns).Consume(Consumer);
+        }
+
+        private static IEnumerable<Cell> DoGetCellsUsingNestedForLoop(int numberOfRowsAndColumns)
+        {
+            for (var columnIndex = 0; columnIndex < numberOfRowsAndColumns; columnIndex++)
+            {
+                for (var rowIndex = 0; rowIndex < numberOfRowsAndColumns; rowIndex++)
+                {
+                    yield return new Cell(columnIndex, rowIndex);
+                }
+            }
+        }
+
+        [ArgumentsSource(nameof(ValuesForNumberOfRowsAndColumns))]
+        [Benchmark]
+#pragma warning disable CA1822
+        public void GetCells_Using_NestedForEachLoop(int numberOfRowsAndColumns)
+#pragma warning restore CA1822
+        {
+            DoGetCellsUsingNestedForEachLoop(numberOfRowsAndColumns).Consume(Consumer);
+        }
+
+        private static IEnumerable<Cell> DoGetCellsUsingNestedForEachLoop(int numberOfRowsAndColumns)
+        {
+            var columnIndices = Enumerable.Range(0, numberOfRowsAndColumns);
+            var rowIndices = Enumerable.Range(0, numberOfRowsAndColumns).ToList();
+
+            foreach (var columnIndex in columnIndices)
+            {
+                foreach (var rowIndex in rowIndices)
+                {
+                    yield return new Cell(columnIndex, rowIndex);
+                }
+            }
+        }
+
+        [ArgumentsSource(nameof(ValuesForNumberOfRowsAndColumns))]
+        [Benchmark]
+#pragma warning disable CA1822
+        public void GetCells_Using_SelectMany(int numberOfRowsAndColumns)
+#pragma warning restore CA1822
+        {
+            var columnIndices = Enumerable.Range(0, numberOfRowsAndColumns);
+            var rowIndices = Enumerable.Range(0, numberOfRowsAndColumns);
+            
+            var cells = columnIndices.SelectMany(_ => rowIndices, (ci, ri) => new Cell(ci, ri));
+            
+            cells.Consume(Consumer);
+        }
+        
+        [ArgumentsSource(nameof(ValuesForNumberOfRowsAndColumns))]
+        [Benchmark]
+#pragma warning disable CA1822
+        public void GetCells_Using_QueryExpression(int numberOfRowsAndColumns)
+#pragma warning restore CA1822
+        {
+            var columnIndices = Enumerable.Range(0, numberOfRowsAndColumns);
+            var rowIndices = Enumerable.Range(0, numberOfRowsAndColumns);
+            
+            var cells = from columnIndex in columnIndices
+                        from rowIndex in rowIndices
+                        select new Cell(columnIndex, rowIndex);
+            
+            cells.Consume(Consumer);
+        }
+    }
+}
