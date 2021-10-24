62f06ba751de6dd0816c939c87d9090397b62a19
Mark Roberts <mroberts@geodesys.com>
2021-10-24 10:14:48 +0100

    Turned Program into top level statements to remove lots of noise
    And remove a Rider warning about `Program` never being instantiated! 😉

--- src/ConsoleUI/Program.cs
+++ src/ConsoleUI/Program.cs
@@ -2,27 +2,18 @@
 using System.IO;
 using Dgt.Minesweeper.Engine;
 
-namespace ConsoleUI
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var input = File.ReadAllLines(@".\Minefield.txt");
-            
-            var generator = new MinefieldGenerator();
-            var minefield = generator.GenerateMinefield(input);
-            
-            var renderer = new MinefieldRenderer();
-            var output = renderer.Render(minefield);
+var input = File.ReadAllLines(@".\Minefield.txt");
 
-            foreach (var line in output)
-            {
-                Console.WriteLine(line);
-            }
-            
-            Console.WriteLine("Press Enter to exit...");
-            Console.ReadLine();
-        }
-    }
+var generator = new MinefieldGenerator();
+var minefield = generator.GenerateMinefield(input);
+
+var renderer = new MinefieldRenderer();
+var output = renderer.Render(minefield);
+
+foreach (var line in output)
+{
+    Console.WriteLine(line);
 }
+
+Console.WriteLine("Press Enter to exit...");
+Console.ReadLine();
