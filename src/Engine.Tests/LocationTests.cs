63e558b224e22c3344dc191867ae8049ff8983c4
Mark Roberts <mroberts@geodesys.com>
2021-10-26 07:07:00 +0100

    Started experimenting with treating e.g. "A1" as a "Location"
    Although we _could_ represent the location as a string we have a proper type; we don't want to fall into the "primitive obsession" trap!
    
    The idea was to remove some of the awkwardness around constantly thinking things like, "Is this index a row or a column?", and "Which order am I supposed to pass these again?".
    
    I could try passing the location in the same format we would use on something like a chess board.
    
    Having a new type would then also free up the name `Cell` which I was starting to think needed to be much more than just the pairing of column and row index.
    
    Some possible downsides of this approach... We'll almost certainly end up putting more strain on the heap because we've replaced one `int` with a `string`. We've also removed the possibility of negative coordinates. Although we could have e.g. "-A-1" (which looks awful IMO) we'd still have no reasonable way to represent the 0 index for column!

---
+++ src/Engine.Tests/LocationTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Minesweeper.Engine
+{
+    public class LocationTests
+    {
+        [Theory]
+        [InlineData("A1", "A", 1)]
+        [InlineData("a1", "A", 1)]
+        [InlineData("H8", "H", 8)]
+        [InlineData("AA99", "AA", 99)]
+        [InlineData("ZZZ1000", "ZZZ", 1000)]
+        public void Parse_Should_ParseColumnAndRowWhenInputIsProperlyFormatted
+            (string input, string expectedColumn, int expectedRow)
+        {
+            // Arrange, Act
+            var (column, row) = Location.Parse(input);
+            
+            // Assert
+            column.Should().Be(expectedColumn);
+            row.Should().Be(expectedRow);
+        }
+
+        [Theory]
+        [InlineData("    A1", "A", 1)]
+        [InlineData("A1     ", "A", 1)]
+        [InlineData("A       1", "A", 1)]
+        [InlineData("   A  1   ", "A", 1)]
+        public void Parse_Should_IgnoreWhitespaceWhenParsingInput
+            (string input, string expectedColumn, int expectedRow)
+        {
+            // Arrange, Act
+            var (column, row) = Location.Parse(input);
+            
+            // Assert
+            column.Should().Be(expectedColumn);
+            row.Should().Be(expectedRow);
+        }
+    }
+}
---
+++ src/Engine/Location.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Dgt.Minesweeper.Engine
+{
+    // IDEA Reduce memory footprint by using ReadOnlySpan<char>
+    // We're likely to be allocating lots of Location instances. We're also likely to only see a small set of values
+    // for Column. Can we consume a lot less memory by keeping a list of spans and then pointing different instances
+    // of Location to the same span?
+    
+    // TODO The constructor needs to validate the format of Column
+    // TODO The constructor needs to validate Row is positive, non-zero
+    public record Location(string Column, int Row)
+    {
+        private const string LocationPattern = @"^\s*(?<column>[A-Z]+)\s*(?<row>\d+)\s*$";
+        private static readonly Regex LocationRegex = new(LocationPattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        
+        // TODO Validate input is not null or empty
+        public static Location Parse(string input)
+        {
+            var match = LocationRegex.Match(input);
+
+            if (!match.Success)
+            {
+                throw new FormatException();
+            }
+
+            var column = match.Groups["column"].Value.ToUpperInvariant();
+            var row = int.Parse(match.Groups["row"].Value);
+
+            return new Location(column, row);
+        }
+    }
+}
