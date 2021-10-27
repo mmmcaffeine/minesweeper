113b5ebf92c38171fef2d73a199d38a0b4ce487a
Mark Roberts <mroberts@geodesys.com>
2021-10-27 07:18:38 +0100

    Implemented ability to convert between int and string representations of columns
    We have _at least_ two scenarios where we need to convert between the two representations - when comparing two columns for adjacency, and when building an `IEnumerable<Location>` based on a number of columns and rows.
    
    At the moment this conversion logic is being split between multiple types. This is starting to bring it into the same place.
    
    At the moment we only deal with a single character and have no exception handling in place.

---
+++ src/Engine.Tests/ColumnConverterTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Minesweeper.Engine
+{
+    public class ColumnConverterTests
+    {
+        public static TheoryData<int, string> ToColumnStringTestData => new()
+        {
+            { 1, "A" },
+            { 5, "E" },
+            { 26, "Z" }
+        };
+        
+        public static TheoryData<string, int> FromColumnStringTestData => new()
+        {
+            { "A", 1 },
+            { "E", 5 },
+            { "Z", 26 }
+        };
+
+        [Theory]
+        [MemberData(nameof(ToColumnStringTestData))]
+        public void ToColumnString_ShouldConvertIntegerToColumnString(int index, string expected) =>
+            index.ToColumnString().Should().Be(expected);
+
+        [Theory]
+        [MemberData(nameof(FromColumnStringTestData))]
+        public void FromColumnString_Should_ConvertColumnStringToInteger(string column, int expected) =>
+            column.FromColumnString().Should().Be(expected);
+    }
+}
---
+++ src/Engine/ColumnConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Dgt.Minesweeper.Engine
+{
+    public static class ColumnConverter
+    {
+        private const int AsciiCodeForA = 65;
+        private const int BaseOneOffset = 1;
+        
+        // TODO 1 or greater
+        public static string ToColumnString(this int value)
+        {
+            var c = (char)(value + AsciiCodeForA - BaseOneOffset);
+
+            return new string(new[] { c });
+        }
+        
+        // TODO No nulls or empty strings
+        // TODO No strings that contain non-chars
+        // TODO Case insensitive
+        public static int FromColumnString(this string value)
+        {
+            return value[0] - AsciiCodeForA + BaseOneOffset;
+        }
+    }
+}
