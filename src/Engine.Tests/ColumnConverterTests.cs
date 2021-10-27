09d4e1adad8f409a6de1affebcca76c08ba3355a
Mark Roberts <mroberts@geodesys.com>
2021-10-27 07:22:21 +0100

    Removed duplicated test data for column conversion

--- src/Engine.Tests/ColumnConverterTests.cs
+++ src/Engine.Tests/ColumnConverterTests.cs
@@ -5,28 +5,23 @@ namespace Dgt.Minesweeper.Engine
 {
     public class ColumnConverterTests
     {
-        public static TheoryData<int, string> ToColumnStringTestData => new()
+        public static TheoryData<int, string> ConversionTestData => new()
         {
             { 1, "A" },
             { 5, "E" },
             { 26, "Z" }
         };
-        
-        public static TheoryData<string, int> FromColumnStringTestData => new()
-        {
-            { "A", 1 },
-            { "E", 5 },
-            { "Z", 26 }
-        };
 
         [Theory]
-        [MemberData(nameof(ToColumnStringTestData))]
+        [MemberData(nameof(ConversionTestData))]
         public void ToColumnString_ShouldConvertIntegerToColumnString(int index, string expected) =>
             index.ToColumnString().Should().Be(expected);
 
+        // We typically have the expected parameter as the last one in the list. However, swapping them around
+        // means we can use the same set of test data for both methods
         [Theory]
-        [MemberData(nameof(FromColumnStringTestData))]
-        public void FromColumnString_Should_ConvertColumnStringToInteger(string column, int expected) =>
-            column.FromColumnString().Should().Be(expected);
+        [MemberData(nameof(ConversionTestData))]
+        public void FromColumnString_Should_ConvertColumnStringToInteger(int expectedIndex, string column) =>
+            column.FromColumnString().Should().Be(expectedIndex);
     }
 }
