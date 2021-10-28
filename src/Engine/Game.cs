ca92bffdb775760740d27df6c11e9548b48af19b
Mark Roberts <mroberts@geodesys.com>
2021-10-28 23:36:19 +0100

    Updated Game to account for Minefield now using Location instead of Cell
    Game itself required barely any changes 😀 More changes were required in the test fixture, but they were all related to the setup of the fake `IMinefield`. This adds further weight to my thought process we'd be better of turning these into "module" tests and just using a real `Minefield`.

--- src/Engine/Game.cs
+++ src/Engine/Game.cs
@@ -7,16 +7,16 @@ namespace Dgt.Minesweeper.Engine
     public class Game
     {
         private readonly IMinefield _minefield;
-        private readonly Dictionary<Cell, CellState> _cellStates = new();
+        private readonly Dictionary<Location, CellState> _cellStates = new();
 
         // TODO Validate no nulls (even though we have enabled Nullable Types)
         public Game(IMinefield minefield)
         {
             _minefield = minefield;
             
-            foreach (var cell in _minefield)
+            foreach (var location in _minefield)
             {
-                _cellStates[cell] = _minefield.IsMined(cell)
+                _cellStates[location] = _minefield.IsMined(location)
                     ? CellState.Mined
                     : CellState.Uncleared;
             }
@@ -25,22 +25,22 @@ namespace Dgt.Minesweeper.Engine
         // TODO Do we want to improve performance by storing a value here, rather than calculating it every time?
         // TODO If we calculate this can we improve this by embedding whether a CellState is for a mined or not mined cell?
         public bool IsWon => _cellStates.Keys
-            .Where(cell => !_minefield.IsMined(cell))
-            .All(cell => _cellStates[cell] == CellState.Cleared);
+            .Where(location => !_minefield.IsMined(location))
+            .All(location => _cellStates[location] == CellState.Cleared);
         
         // TODO Do we want to improve performance by storing a value here, rather than calculating it every time?
         public bool IsLost => _cellStates.Values.Any(state => state == CellState.Exploded);
 
         // TODO Validate the cell exists on the minefield / in our dictionary of cell states
-        public CellState GetCellState(Cell cell) => _cellStates[cell];
+        public CellState GetCellState(Location location) => _cellStates[location];
 
-        // TODO Validate the cell exists on the minefield / in our dictionary of cell states
+        // TODO Validate the location exists in the minefield / in our dictionary of cell states
         // At this point it is looking like we're not getting a whole lot of value by _not_ using an enum
         // if we _don't_ have different classes to represent the different states, then delegate to implementations
         // of e.g. Reveal on them. This switch is harder for people to grok than a switch on an enum
-        public CellState Reveal(Cell cell)
+        public CellState Reveal(Location location)
         {
-            var oldCellState = GetCellState(cell);
+            var oldCellState = GetCellState(location);
             var newCellState = oldCellState switch
             {
                 var s when s == CellState.Uncleared => CellState.Cleared,
@@ -48,7 +48,7 @@ namespace Dgt.Minesweeper.Engine
                 _ => throw new NotImplementedException()
             };
 
-            return _cellStates[cell] = newCellState;
+            return _cellStates[location] = newCellState;
         }
     }
 }
