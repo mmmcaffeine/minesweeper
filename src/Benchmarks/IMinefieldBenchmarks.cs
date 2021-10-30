e323cde9893bfa54ac01f86f6769cdb824a30b27
Mark Roberts <mroberts@geodesys.com>
2021-10-30 11:54:55 +0100

    Added benchmarks for the default implementation of Contains on the IMinefield interface
    Although a little awkward to get at (for now) these benchmarks prove it is definitely worth doing so. Our implementation _massively_ outperforms the LINQ one in _all_ circumstances. Unlike the LINQ version its performance is also linear. The LINQ version is slower based on the passed location, _and_ slows down dramatically as the size of the minefield increases.
    
    I suspect the abysmal performance of the LINQ implementation is due to rebuilding the enumerable on each call. I'm confident it would perform better if e.g. Minefield stashed a List<Location> and returned that each time. However, I'm also confident it would _still_ be slower than our version that implements the test with some arithmetic.

---
+++ src/Benchmarks/IMinefieldBenchmarks.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using Dgt.Minesweeper.Engine;
+
+namespace Dgt.Minesweeper.Benchmarks
+{
+    // ReSharper disable once InconsistentNaming
+    // Resharper isn't keen on the name of this test fixture, and I'm not either. It sounds like it should be an
+    // interface. However, the benchmarks in this type are for default implementations on the IMinefield interface,
+    // hence needing something that is separate from MinefieldTests
+    [MemoryDiagnoser]
+    public class IMinefieldBenchmarks
+    {
+        private class BenchmarkMinefield : IMinefield
+        {
+            public BenchmarkMinefield(int numberOfColumnsAndRows)
+            {
+                NumberOfColumns = numberOfColumnsAndRows;
+                NumberOfRows = numberOfColumnsAndRows;
+            }
+
+            public int NumberOfColumns { get; }
+            public int NumberOfRows { get; }
+            public int CountOfMines => 0;
+            public bool IsMined(Location location) => throw new NotSupportedException();
+            public int GetHint(Location location) => throw new NotSupportedException();
+        }
+        
+        public enum LocationPosition
+        {
+            BottomLeft,
+            Middle,
+            TopRight
+        }
+
+        private IMinefield _minefield = default!;
+        private Location _location = default!;
+        
+        // ReSharper disable MemberCanBePrivate.Global
+        // ReSharper disable UnusedAutoPropertyAccessor.Global
+        // BenchmarkDotNet requires public read / write properties for us to use the Params attributes
+        
+        [Params(10, 100, 1000)]
+        public int NumberOfRowsAndColumns { get; set; }
+        
+        [ParamsAllValues]
+        public LocationPosition Position { get; set; }
+        
+        // ReSharper restore UnusedAutoPropertyAccessor.Global
+        // ReSharper restore MemberCanBePrivate.Global
+        
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _minefield = new BenchmarkMinefield(NumberOfRowsAndColumns);
+            _location = GetLocation();
+        }
+
+        [Benchmark(Baseline = true)]
+        public bool ContainsUsingLinq()
+        {
+            // Cast is required to ensure we get the Linq implementation, not the interface implementation
+            return ((BenchmarkMinefield)_minefield).Contains(_location);
+        }
+        
+        [Benchmark]
+        public bool ContainsUsingDefaultInterfaceImplementation()
+        {
+            return _minefield.Contains(_location);
+        }
+
+        private Location GetLocation()
+        {
+            var index = Position switch
+            {
+                LocationPosition.BottomLeft => 1,
+                LocationPosition.Middle => Math.DivRem(NumberOfRowsAndColumns, 2, out _),
+                LocationPosition.TopRight => NumberOfRowsAndColumns,
+                _ => throw new InvalidOperationException()
+            };
+            
+            return new Location(index, index);
+        }
+    }
+}
