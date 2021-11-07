929e394efafd63cdf9d696adff3eee30a600aa60
Mark Roberts <mroberts@geodesys.com>
2021-11-07 21:11:27 +0000

    Added InvalidMoveException to be thrown when the consumer tries an invalid move
    An invalid move would be something along the lines of trying to reveal a `Cell` they have already revealed.

---
+++ src/Engine.Tests/InvalidMoveExceptionTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Minesweeper.Engine
+{
+    public class InvalidMoveExceptionTests
+    {
+        [Fact]
+        public void Ctor_Should_InitialiseCellWhenCellIsPassed()
+        {
+            // Arrange, Act
+            var location = Location.Parse("A5");
+            var cell = new Cell(location, false, 0);
+            var sut = new InvalidMoveException(cell, location, "foo", "bar");
+            
+            // Assert
+            sut.Cell.Should().Be(cell);
+        }
+        
+        [Fact]
+        public void Ctor_Should_InitialiseLocationWhenCellIsPassed()
+        {
+            // Arrange, Act
+            var location = Location.Parse("A5");
+            var cell = new Cell(location, false, 0);
+            var sut = new InvalidMoveException(cell, location, "foo", "bar");
+            
+            // Assert
+            sut.Location.Should().Be(location);
+        }
+
+        [Fact]
+        public void Ctor_Should_IncludeDefaultMessage()
+        {
+            // Arrange, Act
+            var location = Location.Parse("A5");
+            var cell = new Cell(location, false, 0);
+            var sut = new InvalidMoveException(cell, location, "foo", "bar");
+            
+            // Assert
+            sut.Message.Should().StartWith("The move is not valid due to the current state of the Cell at the given Location.");
+        }
+
+        [Fact]
+        public void Ctor_Should_IncludeLocationAndStateInMessageWhenLocationAndStateArePassed()
+        {
+            // Arrange, Act
+            var location = Location.Parse("A5");
+            var cell = new Cell(location, false, 0);
+            var sut = new InvalidMoveException(cell, location, "revealed", "foo");
+            
+            // Assert
+            sut.Message.Should().Contain("The Cell at Location \"A5\" has been revealed.");
+        }
+
+        [Fact]
+        public void Ctor_Should_IncludeExplanationInMessage()
+        {
+            // Arrange, Act
+            var location = Location.Parse("A5");
+            var cell = new Cell(location, false, 0);
+            var sut = new InvalidMoveException(cell, location, "foo", "You can't do the thing because the reason");
+            
+            // Assert
+            sut.Message.Should().EndWith("You can't do the thing because the reason.");
+        }
+    }
+}
