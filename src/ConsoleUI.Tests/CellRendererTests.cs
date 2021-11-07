db92d28a6f58519efe6fa53781856ddb5a2bad79
Mark Roberts <mroberts@geodesys.com>
2021-11-07 22:21:18 +0000

    Added a test project for the Console UI
    Includes a couple of tests for how we are going to render individual cells, mostly to prove we are able to detect and execute the tests.

---
+++ src/ConsoleUI.Tests/CellRendererTests.cs
@@ -0,0 +1,51 @@
+﻿using Dgt.Minesweeper.Engine;
+using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Minesweeper.ConsoleUI
+{
+    public class CellRendererTests
+    {
+        [Theory]
+        [InlineData(false, 0)]
+        [InlineData(false, 1)]
+        [InlineData(true, 0)]
+        [InlineData(true, 4)]
+        public void RenderCell_Should_RenderNotRevealedAndNotFlaggedCellAsPeriod(bool isMined, int hint)
+        {
+            // Arrange
+            var cell = new Cell(Location.Parse("A1"), isMined, hint)
+            {
+                IsRevealed = false,
+                IsFlagged = false
+            };
+
+            // Act
+            var character = CellRenderer.RenderCell(cell);
+
+            // Assert
+            character.Should().Be('.');
+        }
+
+        [Theory]
+        [InlineData(false, 0)]
+        [InlineData(false, 1)]
+        [InlineData(true, 0)]
+        [InlineData(true, 4)]
+        public void RenderCell_Should_RenderNotRevealedAndFlaggedCellAsF(bool isMined, int hint)
+        {
+            // Arrange
+            var cell = new Cell(Location.Parse("A1"), isMined, hint)
+            {
+                IsRevealed = false,
+                IsFlagged = true
+            };
+
+            // Act
+            var character = CellRenderer.RenderCell(cell);
+
+            // Assert
+            character.Should().Be('F');
+        }
+    }
+}
---
+++ src/ConsoleUI/CellRenderer.cs
@@ -0,0 +1,9 @@
+﻿using Dgt.Minesweeper.Engine;
+
+namespace Dgt.Minesweeper.ConsoleUI
+{
+    public static class CellRenderer
+    {
+        public static char RenderCell(Cell cell) => cell.IsFlagged ? 'F' : '.';
+    }
+}
