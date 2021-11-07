70c9bd74b5d752c045f814a18b765cc719d69c74
Mark Roberts <mroberts@geodesys.com>
2021-11-07 19:02:36 +0000

    Added GameOverException to be thrown when people attempt to make moves after a game has been won or lost

---
+++ src/Engine.Tests/GameOverExceptionTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Minesweeper.Engine
+{
+    public class GameOverExceptionTests
+    {
+        [Fact]
+        public void Ctor_Should_IncludeGameOverInformationInMessage()
+        {
+            // Arrange, Act
+            var sut = new GameOverException(true);
+            
+            // Assert
+            sut.Message.Should().StartWith("The Game is over and no further moves may be made.");
+        }
+        
+        [Fact]
+        public void Ctor_Should_IncludeGameIsWonInformationInMessage()
+        {
+            // Arrange, Act
+            var sut = new GameOverException(true);
+            
+            // Assert
+            sut.Message.Should().EndWith("The Game has already been won.");
+        }
+        
+        [Fact]
+        public void Ctor_Should_IncludeGameIsLostInformationInMessage()
+        {
+            // Arrange, Act
+            var sut = new GameOverException(false);
+            
+            // Assert
+            sut.Message.Should().EndWith("The Game has already been lost.");
+        }
+    }
+}
