1d4eba3d60ac8b66bdbb48d58acdbd9b8e99c83a
Mark Roberts <mroberts@geodesys.com>
2021-11-09 23:00:25 +0000

    Corrected faulty extraction of IRowRenderer interface
    I had the wrong options selected in the tooling, and wasn't paying attention to the diffs when I committed the changes. Sloppy 😢

--- src/ConsoleUI/IRowRenderer.cs
+++ src/ConsoleUI/IRowRenderer.cs
@@ -1,6 +1,15 @@
-﻿namespace Dgt.Minesweeper.ConsoleUI
+﻿using System;
+using System.Collections.Generic;
+using Dgt.Minesweeper.Engine;
+
+namespace Dgt.Minesweeper.ConsoleUI
 {
     public interface IRowRenderer
     {
+        ReadOnlySpan<char> RenderTopBorder(int numberOfRows, int numberOfColumns);
+        ReadOnlySpan<char> RenderRow(int rowIndex, int numberOfRows, IEnumerable<Cell> cells);
+        ReadOnlySpan<char> RenderRowSeparator(int numberOfRows, int numberOfColumns);
+        ReadOnlySpan<char> RenderBottomBorder(int numberOfRows, int numberOfColumns);
+        IEnumerable<string> RenderColumnNames(int numberOfRows, IEnumerable<ColumnName> columnNames);
     }
 }
