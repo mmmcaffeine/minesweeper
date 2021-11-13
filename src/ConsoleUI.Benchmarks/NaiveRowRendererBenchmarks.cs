68a7277a0cc1ce631dfb8f32e01ec3da89d0f0b1
Mark Roberts <mroberts@geodesys.com>
2021-11-13 17:40:01 +0000

    Tried implementation of rendering top border using string.Create
    This implementation is _significantly_ faster than the naive one, especially as the grid gets bigger. It is also more memory efficient.
    
    This is a nice improvement even before we account for an implementation of `IGameRenderer` would be able to cache the response and re-use it; it will never change no matter how many times we render the game.
    
    We may find further improvements with exactly how we're filling the `Span<char>`, and how we're getting the length of the prefix.

--- src/ConsoleUI.Benchmarks/NaiveRowRendererBenchmarks.cs
+++ src/ConsoleUI.Benchmarks/NaiveRowRendererBenchmarks.cs
@@ -10,6 +10,7 @@ namespace Dgt.Minesweeper.ConsoleUI
         {
             public const string NaiveRowRenderer = "NaiveRowRenderer";
             public const string RenderTopBorder = "RenderTopBorder";
+            public const string StringCreate = "StringCreate";
         }
         
         private IRowRenderer _rowRenderer = default!;
@@ -38,5 +39,32 @@ namespace Dgt.Minesweeper.ConsoleUI
         [ArgumentsSource(nameof(ValuesForNumberOfRowsAndColumns))]
         public string RenderTopBorder(int numberOfRowsAndColumns) =>
             _rowRenderer.RenderTopBorder(numberOfRowsAndColumns, numberOfRowsAndColumns);
+
+        [Benchmark]
+        [BenchmarkCategory(BenchmarkCategories.StringCreate, BenchmarkCategories.RenderTopBorder)]
+        [ArgumentsSource(nameof(ValuesForNumberOfRowsAndColumns))]
+#pragma warning disable CA1822
+        public string RenderTopBorder_Using_StringCreate(int numberOfRowsAndColumns)
+#pragma warning restore CA1822
+        {
+            var prefixLength = numberOfRowsAndColumns.ToString().Length;
+            var totalLength = prefixLength + 1 + numberOfRowsAndColumns * 2 + 1;
+
+            return string.Create(totalLength, prefixLength, (span, i) =>
+            {
+                // Would it be more efficient to _not_ pre-fill the span, but fill it within the loop?
+                span.Fill('═');
+
+                span[..(prefixLength + 1)].Fill(' ');
+                span[prefixLength + 1] = '╔';
+
+                for (var x = span.Length - 3; x >= i + 3; x -= 2)
+                {
+                    span[x] = '╦';
+                }
+
+                span[^1] = '╗';
+            });
+        }
     }
 }
