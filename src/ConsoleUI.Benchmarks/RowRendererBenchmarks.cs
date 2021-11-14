0b6f87b1d79b3d1eaea9bdeffe79325beb76b55a
Mark Roberts <mroberts@geodesys.com>
2021-11-14 07:32:48 +0000

    Tried alternative implementations of IRowRenderer.RenderTopBorder
    Manually creating a `char[]` and populating it has proved faster than using `string.Create`, albeit at the cost of more memory and garbage collection as the size of the minefield used by the `Game` increases.
    
    Unsurprisingly, using an `IEnumerable<string>` and `string.Join` performs abysmally both in terms of speed and space.

--- src/ConsoleUI.Benchmarks/RowRendererBenchmarks.cs
+++ src/ConsoleUI.Benchmarks/RowRendererBenchmarks.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using BenchmarkDotNet.Attributes;
 
 namespace Dgt.Minesweeper.ConsoleUI
@@ -12,6 +13,8 @@ namespace Dgt.Minesweeper.ConsoleUI
             public const string EfficientGameRenderer = "EfficientGameRenderer";
             public const string RenderTopBorder = "RenderTopBorder";
             public const string StringCreate = "StringCreate";
+            public const string CharArray = "CharArray";
+            public const string JoinedEnumerableOfString = "JoinedEnumerable";
         }
         
         private IRowRenderer _naiveRowRenderer = default!;
@@ -46,5 +49,49 @@ namespace Dgt.Minesweeper.ConsoleUI
         [ArgumentsSource(nameof(ValuesForNumberOfRowsAndColumns))]
         public string RenderTopBorder_Using_EfficientGameRenderer(int numberOfRowsAndColumns) =>
             _efficientGameRenderer.RenderTopBorder(numberOfRowsAndColumns, numberOfRowsAndColumns);
+
+        [Benchmark]
+        [BenchmarkCategory(BenchmarkCategories.CharArray, BenchmarkCategories.RenderTopBorder)]
+        [ArgumentsSource(nameof(ValuesForNumberOfRowsAndColumns))]
+#pragma warning disable CA1822
+        public string RenderTopBorder_Using_CharArray(int numberOfRowsAndColumns)
+#pragma warning restore CA1822
+        {
+            var prefixLength = numberOfRowsAndColumns.ToString().Length;
+            var totalLength = prefixLength + 1 + numberOfRowsAndColumns * 2 + 1;
+            var chars = new char[totalLength];
+
+            for (var i = 0; i <= prefixLength + 1; i++)
+            {
+                chars[i] = ' ';
+            }
+
+            chars[prefixLength + 1] = '╔';
+
+            for (var i = chars.Length - 3; i >= prefixLength + 3; i -= 2)
+            {
+                chars[i] = '╦';
+                chars[i - 1] = '═';
+            }
+
+            chars[^2] = '═';
+            chars[^1] = '╗';
+
+            return new string(chars);
+        }
+
+        [Benchmark]
+        [BenchmarkCategory(BenchmarkCategories.JoinedEnumerableOfString, BenchmarkCategories.RenderTopBorder)]
+        [ArgumentsSource(nameof(ValuesForNumberOfRowsAndColumns))]
+#pragma warning disable CA1822
+        public string RenderTopBorder_Using_JoinedEnumerableOfString(int numberOfRowsAndColumns)
+#pragma warning restore CA1822
+        {
+            var prefixLength = numberOfRowsAndColumns.ToString().Length;
+            var columns = Enumerable.Range(0, numberOfRowsAndColumns - 1).Select(_ => "═╦");
+            var prefix = Enumerable.Range(0, prefixLength).Select(_ => ' ');
+
+            return $"{string.Join(null, prefix)} ╔{string.Join(null, columns)}═╗";
+        }
     }
 }
