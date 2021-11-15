4923c72b9790c6cf43585cca520013a34ed90cab
Mark Roberts <mroberts@geodesys.com>
2021-11-15 22:55:37 +0000

    Added benchmarks for implementations of IGameRenderer
    This has show that for now we don't see much difference in performance between the two 😢 This is in spite of us knowing the `IRowRenderer` implementation in `StringCreateRenderer` is much faster than `NaiveRowRenderer`; we've benchmarked them. We also know we have far fewer operations because we only render the row separator once for `StringCreateRenderer`, rather than for every row in `NaiveGameRenderer`.
    
    The lack of difference in speed has been tracked down to the bulk of the execution time coming from enumerating the `ColumnName`s and `Cell`s via the passed instance of `Game`.
    
    On the plus side `StringCreateGameRenderer` is consuming a _tiny_ bit less memory! 😂
    
    Approximately 14 seconds to render a 1,000 x 1,000 grid is not acceptable. Just over 1/10 of a second to render a 100 x 100 grid just about it.

---
+++ src/ConsoleUI.Benchmarks/GameRendererBenchmarks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using Dgt.Minesweeper.Engine;
+
+namespace Dgt.Minesweeper.ConsoleUI
+{
+    [MemoryDiagnoser]
+    public class GameRendererBenchmarks
+    {
+        private IGameRenderer _naiveGameRenderer = default!;
+        private IGameRenderer _stringCreateGameRenderer = default!;
+        private Consumer _consumer = default!;
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        // BenchmarkDotNet requires public read / write properties for us to use the Params attributes
+        [Params(2, 10, 100, 1000)]
+        public int NumberOfRowsAndColumns { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetUp()
+        {
+            var minefield = new Minefield(NumberOfRowsAndColumns, Array.Empty<Location>());
+            var game = new Game(minefield);
+            var cellRenderer = new CellRenderer();
+
+            _naiveGameRenderer = new NaiveGameRenderer(game, new NaiveRowRenderer(), cellRenderer);
+            _stringCreateGameRenderer = new StringCreateGameRenderer(game, cellRenderer);
+            _consumer = new Consumer();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Render_Using_NaiveGameRenderer() => _naiveGameRenderer.Render().Consume(_consumer);
+
+        [Benchmark]
+        public void Render_Using_StringCreateGameRenderer() => _stringCreateGameRenderer.Render().Consume(_consumer);
+    }
+}
